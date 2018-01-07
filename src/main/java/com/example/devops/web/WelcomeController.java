7f28e8844a665fa901f8c93801fb2b7f2d3a49a7
azalbargi <azalbargi.c@stc.com.sa>
2018-01-07 11:42:15 +0300

    Test

--- src/main/java/com/example/devops/web/WelcomeController.java
+++ src/main/java/com/example/devops/web/WelcomeController.java
@@ -14,7 +14,7 @@ public class WelcomeController {
 	@RequestMapping("/")
 	public String welcome(Model model) {
 		logger.info("Proccessing index request");
-		model.addAttribute("course", "DevOps T");
+		model.addAttribute("course", "DevOps");
 		return "index";
 	}
 }
